1899ed6d6bb6a1e30bc6724346b6f4a652cd9b34
NomBlayi <nomblayi33@gmail.com>
2023-03-16 14:57:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-#include <unstd.h>
-/*
- * main - C program that prints a line without using any functions 
+#include <unistd.h>
+
+/**
+ * main - C program that prints a line without using any functions
  * listed in the NAME section of the man (3) printf or man (3) puts
- * Return 1 (Success)
+ * Return: 1 (Success)
  */
-int main (void)
+
+int main(void)
+
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+return (1);
+
 }
 
