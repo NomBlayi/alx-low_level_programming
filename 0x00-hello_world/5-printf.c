b521c1dd34fd810c77eff5154d119b861faab024
NomBlayi <nomblayi33@gmail.com>
2023-03-16 06:37:21 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - C program that prints a line using a printf function and no puts function allowed
+ * Return: 0 (Success)
+ * */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
