65217b5ba1bbb858128d93d1a2866fda769241a7
NomBlayi <nomblayi33@gmail.com>
2023-03-17 14:10:15 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	putchar(b);
+	putchar('\n');
+	return (0);
+}
+
