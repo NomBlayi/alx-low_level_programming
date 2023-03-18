f334f5bba3fb4f4ee9e3d5e3f25e9c46a4814f38
NomBlayi <nomblayi33@gmail.com>
2023-03-17 17:04:17 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A program that prints all possible
+ * different combinations of two digits
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+	for (b = a + 1; b <= '9'; b++)
+	{
+	if (b != a)
+	{
+	putchar(a);
+	putchar(b);
+	if (a == '0' && b == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+
