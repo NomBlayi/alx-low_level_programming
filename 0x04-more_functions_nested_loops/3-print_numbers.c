4ed49a3fe2397ff2ceb606a7c7a3b42308d7f9e1
NomBlayi <nomblayi33@gmail.com>
2023-03-23 13:16:21 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,17 @@
 
 /**
  * print_numbers -A function that prints the numbers, from 0 to 9
- * Return: printed number from 0 to 9
+ * Return: number from 0 to 9
  */
 
 void print_numbers(void);
 {
 	int i;
 
-	for (i >= 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 	_putchar(i + '0');
 	}
 	_putchar('\n');
+}
+
