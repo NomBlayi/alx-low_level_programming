33ceb5c8aa5b842ac14044ee3913115e5545f165
NomBlayi <nomblayi33@gmail.com>
2023-03-23 16:46:10 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,7 +8,7 @@
 
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size <= 0)
 	{
@@ -18,11 +18,11 @@ void print_triangle(int size)
 	{
 	for (i = 0; i <= size; i++)
 	{
-	for (j = 0; j < size; j++)
+	for (j = size - i; j > 1; j--)
 	{
 	_putchar(' ');
 	}
-	for (j = 0; j <= i; j++)
+	for (k = 0; k <= i; k++)
 	{
 	_putchar('#');
 	}
