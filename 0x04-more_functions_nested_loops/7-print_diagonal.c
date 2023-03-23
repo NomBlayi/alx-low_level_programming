9b2ffa81d8edf60cb1a5a0096595a566c55ca65b
NomBlayi <nomblayi33@gmail.com>
2023-03-23 14:58:03 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,7 +12,7 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 	{
-	_putchar('\n')
+	_putchar('\n');
 	}
 	else
 	{
@@ -20,7 +20,7 @@ void print_diagonal(int n)
 	{
 	for (j = 0; j < i; j++)
 	{
-	_putchar(31);
+	_putchar(32);
 	}
 	_putchar(92);
 	_putchar('\n');
