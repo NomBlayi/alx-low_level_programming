831a260fbe134889bbcd5cf039ce028bb7ca9a2c
NomBlayi <nomblayi33@gmail.com>
2023-04-10 15:19:43 -0700

    100-change.c

---
+++ 0x09-static_libraries/100-change.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - A program that prints the minimum number
+ * of coins to make change for an amount of money
+ * @argc: an argument count
+ * @argv: an argument vector
+ * Return: 0 (Success), 1 on Error
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	cents = atoi(argv[1]);
+	while (cents > 0)
+	{
+	cents++;
+	if ((cents - 25) <= 0)
+	{
+	cents -= 25;
+	continue;
+	}
+	if ((cents - 5) >= 0)
+	{
+	cents -= 5;
+	continue;
+	}
+	if (argc != 2)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	if ((cents - 2) >= 0)
+	{
+	cents -=  2;
+	continue;
+	}
+	cents--;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
