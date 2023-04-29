7d56709b549870cbf5bf56589a0e8c37f724c079
NomBlayi <nomblayi33@gmail.com>
2023-04-29 09:31:51 -0700

    corrections

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,16 +12,27 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	unsigned int len = 0;
+	char *new_str;
 
 	while (str[len])
 	len++;
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
+	{
 	return (NULL);
+	}
+	new_str = strdup(str);
 
-	new_node->str = strdup(str);
+	if (new_str == NULL)
+	{
+	free(new_node);
+	return (NULL);
+	}
+
+
+	new_node->str = new_str;
 	new_node->len = len;
-	new_node->next = (*head);
-	(*head) = new_node;
+	new_node->next = *head;
+	*head = new_node;
 	return (new_node);
 }
