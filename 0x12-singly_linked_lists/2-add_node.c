701589ccdd8d051110a5a5224fcbb3f40ec41879
NomBlayi <nomblayi33@gmail.com>
2023-04-29 08:17:51 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,12 +16,12 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[len])
 	len++;
 	new_node = malloc(sizeof(list_t));
-	if (!new_node)
+	if (new_node == NULL)
 	return (NULL);
 
 	new_node->str = strdup(str);
 	new_node->len = len;
 	new_node->next = (*head);
 	(*head) = new_node;
-	return (*head);
+	return (new_node);
 }
